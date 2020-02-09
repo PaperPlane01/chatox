9596ff51f69ce2fee3cecdf1b7dae1196b08ce78
PaperPlane01 <dagfortnewage@gmail.com>
2020-02-09 14:05:42 +0600

    fix: (09.02.20) Fixed access token and refresh token not being sent upon token revokation request

--- client/src/api/clients/UserApi.ts
+++ client/src/api/clients/UserApi.ts
@@ -56,6 +56,9 @@ export class UserApi {
             headers: {
                 "Content-Type": "application/json",
                 Accept: "application/json"
+            },
+            data: {
+                ...revokeTokenRequest
             }
         })
     }
