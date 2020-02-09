c25402652d31d575fe3b13987b9cc95f3dba72aa
PaperPlane01 <dagfortnewage@gmail.com>
2020-02-09 18:26:18 +0600

    fix: (09.02.20) Fixed current user not being fetched if access token is present (#5, #13)
    Closes #13

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -25,6 +25,10 @@ ReactDOM.render(
     document.getElementById("root")
 );
 
+if (localStorage.getItem("accessToken")) {
+    store.authorization.fetchCurrentUser();
+}
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
