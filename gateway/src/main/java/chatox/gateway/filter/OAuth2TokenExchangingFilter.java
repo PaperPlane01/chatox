3b3ebeb72ce3e8a3218c9240a18cb0dba1842837
PaperPlane01 <dagfortnewage@gmail.com>
2020-02-18 10:01:57 +0600

    feat: (18.02.20) Implemented basic logic of passing JWT token to events service upon handshake (#11)

--- gateway/src/main/java/chatox/gateway/filter/OAuth2TokenExchangingFilter.java
+++ gateway/src/main/java/chatox/gateway/filter/OAuth2TokenExchangingFilter.java
@@ -15,18 +15,32 @@ public class OAuth2TokenExchangingFilter implements WebFilter {
     private TokenExchanger tokenExchanger;
 
     private static final String OAUTH2_SERVICE_URL = "/oauth";
+    private static final String EVENTS_SERVICE_URL = "/api/v1/events";
 
     @Override
     public Mono<Void> filter(ServerWebExchange serverWebExchange, WebFilterChain webFilterChain) {
         var path = serverWebExchange.getRequest().getPath().toString();
 
-        if (!path.startsWith(OAUTH2_SERVICE_URL)) {
+        if (!path.startsWith(OAUTH2_SERVICE_URL) && !path.startsWith(EVENTS_SERVICE_URL)) {
             if (serverWebExchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
                 var accessToken = serverWebExchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
                 var jwt = tokenExchanger.exchangeAccessTokenToJwtToken(accessToken.replace("Bearer ", ""));
                 var request = serverWebExchange.getRequest()
                         .mutate()
                         .headers(httpHeaders -> httpHeaders.set(HttpHeaders.AUTHORIZATION, "Bearer " + jwt))
+                        .build();
+                var exchange = serverWebExchange.mutate()
+                        .request(request)
+                        .build();
+                return webFilterChain.filter(exchange);
+            }
+        } else if (path.startsWith(EVENTS_SERVICE_URL)) {
+            if (serverWebExchange.getRequest().getQueryParams().containsKey("accessToken")) {
+                var accessToken = serverWebExchange.getRequest().getQueryParams().getFirst("accessToken");
+                var jwt = tokenExchanger.exchangeAccessTokenToJwtToken(accessToken);
+                var request = serverWebExchange.getRequest()
+                        .mutate()
+                        .contextPath(EVENTS_SERVICE_URL + "?accessToken=" + jwt)
                         .build();
                 var exchange = serverWebExchange.mutate()
                         .request(request)
