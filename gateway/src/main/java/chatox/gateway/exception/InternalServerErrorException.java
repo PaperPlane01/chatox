82c3938365627349249c0303a5f8ac9c68205924
PaperPlane01 <dagfortnewage@gmail.com>
2020-07-23 05:55:35 +0600

    refactor: (23.07.20) Migrated to WebClient in API gateway
    Closes #10

---
+++ gateway/src/main/java/chatox/gateway/exception/InternalServerErrorException.java
@@ -0,0 +1,19 @@
+package chatox.gateway.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class InternalServerErrorException extends RuntimeException {
+    public InternalServerErrorException() {
+        super("Internal server error");
+    }
+
+    public InternalServerErrorException(String message) {
+        super(message);
+    }
+
+    public InternalServerErrorException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
--- gateway/src/main/java/chatox/gateway/service/TokenExchanger.java
+++ gateway/src/main/java/chatox/gateway/service/TokenExchanger.java
@@ -1,50 +1,59 @@
 package chatox.gateway.service;
 
 import chatox.gateway.exception.AccessTokenExpiredException;
+import chatox.gateway.exception.InternalServerErrorException;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class TokenExchanger {
     private final LoadBalancerClient loadBalancerClient;
 
     private static final String OAUTH2_SERVICE_ID = "oauth2-service";
     private static final String EXCHANGE_TOKEN = "oauth/exchangeToken";
 
-    public String exchangeAccessTokenToJwtToken(String accessToken) {
+    public Mono<String> exchangeAccessTokenToJwtToken(String accessToken) {
         var oauth2ServiceInstance = loadBalancerClient.choose(OAUTH2_SERVICE_ID);
         var url = oauth2ServiceInstance.getUri().toString();
         var exchangeTokenRequest = new ExchangeTokenRequest(accessToken);
 
-        RestTemplate restTemplate = new RestTemplate();
+        var webClient = WebClient.create(url + "/" + EXCHANGE_TOKEN);
 
-        try {
-            ResponseEntity<ExchangeTokenResponse> response = restTemplate.postForEntity(
-                    url + "/" + EXCHANGE_TOKEN,
-                    exchangeTokenRequest,
-                    ExchangeTokenResponse.class
-            );
+        return webClient
+                .post()
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(exchangeTokenRequest), ExchangeTokenRequest.class)
+                .exchange()
+                .doOnNext(clientResponse -> {
+                    if (clientResponse.statusCode().isError()) {
+                        if (clientResponse.statusCode().equals(HttpStatus.UNAUTHORIZED)) {
+                            throw new AccessTokenExpiredException("Access token is either expired or invalid");
+                        }
 
-            return response.getBody().getJwt();
-        } catch (HttpClientErrorException exception) {
+                        log.error(String.format(
+                                "Unexpected error occurred when tried to exchange access token: OAuth2 service responded with status %d",
+                                clientResponse.statusCode().value()
+                        ));
 
-            if (exception.getStatusCode().equals(HttpStatus.UNAUTHORIZED)) {
-                throw new AccessTokenExpiredException("Access token is either invalid or expired");
-            }
+                        throw new InternalServerErrorException();
+                    }
+                })
+                .flatMap(clientResponse -> clientResponse.bodyToMono(ExchangeTokenResponse.class))
+                .map(ExchangeTokenResponse::getJwt);
 
-            exception.printStackTrace();
-            throw exception;
-        }
     }
 
     @Data
