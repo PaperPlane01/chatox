0168a49847c280bcf67dd0bdfd75fd259b2aa14d
PaperPlane01 <dagfortnewage@gmail.com>
2020-08-12 19:35:24 +0600

    refactor: (12.08.20) Migrated localization module to use hooks (#70)

--- client/src/localization/components/LanguagePicker.tsx
+++ client/src/localization/components/LanguagePicker.tsx
@@ -1,58 +1,49 @@
 import React, {FunctionComponent} from "react";
-import {inject, observer} from "mobx-react";
-import {Card, CardContent, CardHeader, FormControl, FormControlLabel, Radio, RadioGroup} from "@material-ui/core";
-import {localized, Localized} from "./localized";
+import {observer} from "mobx-react";
+import {Card, CardContent, CardHeader, FormControlLabel, Radio, RadioGroup} from "@material-ui/core";
 import {Language} from "../types";
 import {LanguageLabel} from "./LanguageLabel";
-import {MapMobxToProps} from "../../store";
+import {useLocalization, useStore} from "../../store";
 
-interface LanguagePickerOwnProps {
+interface LanguagePickerProps {
     hideHeader?: boolean
 }
 
-interface LanguagePickerMobxProps {
-    setLanguage: (language: Language) => void
-}
+export const LanguagePicker: FunctionComponent<LanguagePickerProps> = observer(({
+    hideHeader = false
+}) => {
+    const {
+        language: {
+            setLanguage
+        }
+    } = useStore();
+    const {l, locale} = useLocalization();
 
-type LanguagePickerProps = LanguagePickerOwnProps & LanguagePickerMobxProps & Localized;
-
-const _LanguagePicker: FunctionComponent<LanguagePickerProps> = ({
-    hideHeader = false,
-    setLanguage,
-    locale,
-    l
-}) => (
-    <Card>
-        {!hideHeader && <CardHeader title={l("language.select-language")}/>}
-        <CardContent>
-            <RadioGroup value={locale}
-                        onChange={event => setLanguage(event.target.value as Language)}
-            >
-                <FormControlLabel control={<Radio/>}
-                                  value="en"
-                                  label={
-                                      <LanguageLabel languageLabel={l("language.english")}
-                                                     nativeLanguageLabel={l("language.english.native")}
-                                      />
-                                  }
-                />
-                <FormControlLabel control={<Radio/>}
-                                  value="ru"
-                                  label={
-                                      <LanguageLabel languageLabel={l("language.russian")}
-                                                     nativeLanguageLabel={l("language.russian.native")}
-                                      />
-                                  }
-                />
-            </RadioGroup>
-        </CardContent>
-    </Card>
-);
-
-const mapMobxToProps: MapMobxToProps<LanguagePickerMobxProps> = ({language}) => ({
-    setLanguage: language.setLanguage
+    return (
+        <Card>
+            {!hideHeader && <CardHeader title={l("language.select-language")}/>}
+            <CardContent>
+                <RadioGroup value={locale}
+                            onChange={event => setLanguage(event.target.value as Language)}
+                >
+                    <FormControlLabel control={<Radio/>}
+                                      value="en"
+                                      label={
+                                          <LanguageLabel languageLabel={l("language.english")}
+                                                         nativeLanguageLabel={l("language.english.native")}
+                                          />
+                                      }
+                    />
+                    <FormControlLabel control={<Radio/>}
+                                      value="ru"
+                                      label={
+                                          <LanguageLabel languageLabel={l("language.russian")}
+                                                         nativeLanguageLabel={l("language.russian.native")}
+                                          />
+                                      }
+                    />
+                </RadioGroup>
+            </CardContent>
+        </Card>
+    )
 });
-
-export const LanguagePicker = localized(
-    inject(mapMobxToProps)(observer(_LanguagePicker))
-) as FunctionComponent<LanguagePickerOwnProps>;
