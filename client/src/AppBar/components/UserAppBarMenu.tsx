a9959edbde5638bb2e3901642598bba4aa1d55c4
PaperPlane01 <dagfortnewage@gmail.com>
2020-08-26 23:04:33 +0600

    fix: (26.08.20) Minor fixes to styling of UserAppBarMenu

--- client/src/AppBar/components/UserAppBarMenu.tsx
+++ client/src/AppBar/components/UserAppBarMenu.tsx
@@ -1,22 +1,38 @@
 import React, {Fragment, FunctionComponent, useState} from "react";
 import {observer} from "mobx-react";
-import {Button, IconButton, Menu} from "@material-ui/core";
+import {Button, createStyles, Hidden, IconButton, makeStyles, Menu, Theme} from "@material-ui/core";
 import {Skeleton} from "@material-ui/lab";
-import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import {AccountCircle} from "@material-ui/icons";
 import randomColor from "randomcolor";
 import {RegistrationDialog, RegistrationMenuItem} from "../../Registration";
 import {LoginDialog, LoginMenuItem, LogOutMenuItem} from "../../Authorization";
 import {Avatar} from "../../Avatar";
 import {useAuthorization} from "../../store";
 
+const useStyles = makeStyles((theme: Theme) => createStyles({
+    userButton: {
+        textTransform: "none"
+    },
+    userAvatarContainer: {
+        [theme.breakpoints.up("md")]: {
+            paddingRight: theme.spacing(2)
+        }
+    }
+}));
+
 export const UserAppBarMenu: FunctionComponent = observer(() => {
     const [anchorElement, setAnchorElement] = useState<Element | null>(null);
     const {currentUser, fetchingCurrentUser} = useAuthorization();
+    const classes = useStyles();
 
     if (fetchingCurrentUser) {
         return (
             <Fragment>
-                <Skeleton variant="circle" height={40} width={40}/>
+                <Skeleton variant="circle"
+                          height={40}
+                          width={40}
+                          className={classes.userAvatarContainer}
+                />
                 <Skeleton variant="text" height={20} width={50}/>
             </Fragment>
         )
@@ -31,15 +47,18 @@ export const UserAppBarMenu: FunctionComponent = observer(() => {
             <Fragment>
                 <Button onClick={event => setAnchorElement(event.currentTarget)}
                         color="inherit"
+                        className={classes.userButton}
                 >
-                    <div style={{marginRight: 14}}>
+                    <div className={classes.userAvatarContainer}>
                         <Avatar avatarLetter={avatarLetter}
                                 avatarColor={randomColor({seed: currentUser.id})}
                                 avatarUri={currentUser.avatarUri}
                                 avatarId={currentUser.avatarId}
                         />
                     </div>
-                    {currentUser.firstName}{currentUser.lastName && ` ${currentUser.lastName}`}
+                    <Hidden smDown>
+                        {currentUser.firstName}{currentUser.lastName && ` ${currentUser.lastName}`}
+                    </Hidden>
                 </Button>
                 <Menu open={Boolean(anchorElement)}
                       anchorEl={anchorElement}
@@ -55,7 +74,7 @@ export const UserAppBarMenu: FunctionComponent = observer(() => {
                 <IconButton color="inherit"
                             onClick={event => setAnchorElement(event.currentTarget)}
                 >
-                    <AccountCircleIcon/>
+                    <AccountCircle/>
                 </IconButton>
                 <Menu open={Boolean(anchorElement)}
                       anchorEl={anchorElement}
