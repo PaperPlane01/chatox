e6e911f80c604e7dd5fab5b5c6cdbc70b880aa3b
PaperPlane01 <dagfortnewage@gmail.com>
2020-09-05 18:22:26 +0600

    fix: (05.09.20) Validation of chat was not working

--- chat/src/main/kotlin/chatox/chat/support/validation/annotation/StringNotIn.kt
+++ chat/src/main/kotlin/chatox/chat/support/validation/annotation/StringNotIn.kt
@@ -2,11 +2,16 @@ package chatox.chat.support.validation.annotation
 
 import chatox.chat.support.validation.validator.StringNotInValidator
 import javax.validation.Constraint
+import javax.validation.Payload
+import kotlin.reflect.KClass
 
 @Target(allowedTargets = [AnnotationTarget.FIELD])
 @Retention(value = AnnotationRetention.RUNTIME)
 @Constraint(validatedBy = [StringNotInValidator::class])
 annotation class StringNotIn(
         val value: Array<String> = [],
-        val message: String = "This value is not allowed"
+        val message: String = "This value is not allowed",
+
+        val groups: Array<KClass<*>> = [],
+        val payload: Array<KClass<out Payload>> = []
 )
