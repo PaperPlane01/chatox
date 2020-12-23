39ebdb0971caff845210a78fdfbe83ad6065fcc9
PaperPlane01 <dagfortnewage@gmail.com>
2020-12-24 01:13:02 +0600

    fix: (24.12.20) Fixed global ban was not being mapped properly in GlobalBanMapper

--- oauth2/src/main/java/chatox/oauth2/mapper/GlobalBanMapper.java
+++ oauth2/src/main/java/chatox/oauth2/mapper/GlobalBanMapper.java
@@ -3,6 +3,7 @@ package chatox.oauth2.mapper;
 import chatox.oauth2.domain.GlobalBan;
 import chatox.oauth2.messaging.rabbitmq.event.GlobalBanCreatedOrUpdated;
 import chatox.oauth2.respository.AccountRepository;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -22,8 +23,10 @@ public abstract class GlobalBanMapper {
     })
     public abstract GlobalBan fromGlobalBanCreatedOrUpdated(GlobalBanCreatedOrUpdated globalBanCreatedOrUpdated);
 
+    @AfterMapping
     protected void afterMapping(GlobalBanCreatedOrUpdated globalBanCreatedOrUpdated,
                                 @MappingTarget GlobalBan globalBan) {
-        globalBan.setBannedAccount(accountRepository.findByUserIdsContains(globalBanCreatedOrUpdated.getBannedUser().getId()));
+        var banedAccount = accountRepository.findByUserIdsContains(globalBanCreatedOrUpdated.getBannedUser().getId());
+        globalBan.setBannedAccount(banedAccount);
     }
 }
