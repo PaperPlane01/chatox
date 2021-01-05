ff23bfc33771302fc4b738cbb9ad312f56fdf63c
PaperPlane01 <dagfortnewage@gmail.com>
2021-01-05 21:18:35 +0600

    fix: (05.01.21) Fixed dependency injection issue in ChatMongoEventListener (#126)

--- chat/src/main/kotlin/chatox/chat/mongo/listener/ChatMongoEventListener.kt
+++ chat/src/main/kotlin/chatox/chat/mongo/listener/ChatMongoEventListener.kt
@@ -2,12 +2,14 @@ package chatox.chat.mongo.listener
 
 import chatox.chat.model.Chat
 import chatox.platform.cache.ReactiveCacheService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener
 import org.springframework.data.mongodb.core.mapping.event.AfterSaveEvent
 import org.springframework.stereotype.Component
 
 @Component
 class ChatMongoEventListener : AbstractMongoEventListener<Chat>() {
+    @Autowired
     private lateinit var chatCache: ReactiveCacheService<String, Chat>
 
     override fun onAfterSave(event: AfterSaveEvent<Chat>) {
