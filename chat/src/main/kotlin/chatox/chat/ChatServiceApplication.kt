4fbc9b9c8f16829a40ce2539f16b7504ac6824dc
PaperPlane01 <dagfortnewage@gmail.com>
2021-01-05 11:46:26 +0600

    fix: (05.11.21) Removed unused code from ChatServiceApplication

--- chat/src/main/kotlin/chatox/chat/ChatServiceApplication.kt
+++ chat/src/main/kotlin/chatox/chat/ChatServiceApplication.kt
@@ -1,7 +1,5 @@
 package chatox.chat
 
-import chatox.chat.repository.ChatParticipationRepository
-import chatox.chat.repository.MessageRepository
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.mongodb.config.EnableMongoAuditing
@@ -11,19 +9,5 @@ import org.springframework.data.mongodb.config.EnableMongoAuditing
 class ChatServiceApplication
 
 fun main(args: Array<String>) {
-    val applicationContext = runApplication<ChatServiceApplication>(*args)
-    val chatParticipationRepository = applicationContext.getBean(ChatParticipationRepository::class.java)
-
-    val chatParticipations = chatParticipationRepository.findAll().collectList().block()!!
-
-    for (chatParticipation in chatParticipations) {
-        chatParticipationRepository.save(chatParticipation.copy(
-                userNoDbRef = chatParticipation.user,
-                lastActiveChatBlockingId = if (chatParticipation.lastChatBlocking != null) chatParticipation.lastChatBlocking!!.id else null,
-                deletedById = if (chatParticipation.deletedBy != null) chatParticipation.deletedBy!!.id else null,
-                chatId = chatParticipation.chat.id,
-                lastReadMessageId = if (chatParticipation.lastMessageRead != null) chatParticipation.lastMessageRead!!.message.id else null
-        ))
-                .block()
-    }
+    runApplication<ChatServiceApplication>(*args)
 }
