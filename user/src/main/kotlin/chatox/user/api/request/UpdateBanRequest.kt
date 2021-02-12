8cd268da03675d5340fe95eeaae18caf4c2608cb
PaperPlane01 <dagfortnewage@gmail.com>
2021-02-12 23:09:19 +0600

    fix: (12.02.21) Fixed UpdateBanRequest validation error (#58)

--- user/src/main/kotlin/chatox/user/api/request/UpdateBanRequest.kt
+++ user/src/main/kotlin/chatox/user/api/request/UpdateBanRequest.kt
@@ -3,6 +3,7 @@ package chatox.user.api.request
 import chatox.user.domain.GlobalBanReason
 import chatox.user.support.validation.globalban.annotation.RequireCommentIfGlobalBanReasonIs
 import chatox.user.support.validation.globalban.annotation.RequireExpirationDateIfBanIsNotPermanent
+import com.fasterxml.jackson.annotation.JsonProperty
 import java.time.ZonedDateTime
 import javax.validation.constraints.Future
 import javax.validation.constraints.NotNull
@@ -14,7 +15,8 @@ data class UpdateBanRequest(
         val expiresAt: ZonedDateTime?,
 
         @field:NotNull
-        val _reason: GlobalBanReason?,
+        @field:JsonProperty("reason")
+        private val _reason: GlobalBanReason?,
         val permanent: Boolean = false,
         val comment: String?
 ) {
