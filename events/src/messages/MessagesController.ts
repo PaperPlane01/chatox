a42f1d1a1d15d825e15bb5d9bc7ecb00ae023f9b
PaperPlane01 <dagfortnewage@gmail.com>
2021-03-02 12:17:22 +0600

    feat: (02.03.21) Added MESSAGE_PINNED and MESSAGE_UNPINNED events to websocket (#68)

--- events/src/messages/MessagesController.ts
+++ events/src/messages/MessagesController.ts
@@ -2,8 +2,7 @@ import {Injectable} from "@nestjs/common";
 import {RabbitSubscribe} from "@nestjs-plus/rabbitmq";
 import {WebsocketEventsPublisher} from "../websocket";
 import {ChatMessage} from "../common/types";
-import {MessageDeleted} from "../websocket/types";
-import {MessagesDeleted} from "../websocket/types/MessagesDeleted";
+import {MessageDeleted, MessagesDeleted} from "../websocket/types";
 import {config} from "../env-config";
 import {LoggerFactory} from "../logging";
 
@@ -51,5 +50,27 @@ export class MessagesController {
         this.log.debug("Messages deleted event");
         this.log.debug(messagesDeleted);
         await this.websocketEventsPublisher.publishMessagesDeleted(messagesDeleted);
+    }
+
+    @RabbitSubscribe({
+        exchange: "chat.events",
+        queue: `events_service_message_pinned-${config.EVENTS_SERVICE_PORT}`,
+        routingKey: "chat.message.pinned.#"
+    })
+    public async onMessagePinned(message: ChatMessage): Promise<void> {
+        this.log.debug("Message pinned event");
+        this.log.debug(message);
+        await this.websocketEventsPublisher.publishMessagePinned(message);
+    }
+
+    @RabbitSubscribe({
+        exchange: "chat.events",
+        queue: `events_service_message_unpinned-${config.EVENTS_SERVICE_PORT}`,
+        routingKey: "chat.message.unpinned.#"
+    })
+    public async onMessageUnpinned(message: ChatMessage): Promise<void> {
+        this.log.debug("Message unpinned event");
+        this.log.debug(message);
+        await this.websocketEventsPublisher.publishMessageUnpinned(message);
     }
 }
