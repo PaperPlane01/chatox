4879efa8359363b41057317451730bcb1df666e9
PaperPlane01 <dagfortnewage@gmail.com>
2021-06-06 11:06:40 +0600

    feat: (06.06.21) Added proper pagination parameters validation in GET /api/v1/stickers-packs endpoint

--- sticker/src/main/kotlin/chatox/sticker/config/PlatformConfig.kt
+++ sticker/src/main/kotlin/chatox/sticker/config/PlatformConfig.kt
@@ -1,7 +1,11 @@
 package chatox.sticker.config
 
 import chatox.platform.pagination.process.PaginationParametersProcessor
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
+@Configuration
 class PlatformConfig {
+    @Bean
     fun paginationParametersProcessor() = PaginationParametersProcessor()
 }
--- sticker/src/main/kotlin/chatox/sticker/controller/StickerPackController.kt
+++ sticker/src/main/kotlin/chatox/sticker/controller/StickerPackController.kt
@@ -1,10 +1,12 @@
 package chatox.sticker.controller
 
 import chatox.platform.pagination.PaginationRequest
+import chatox.platform.pagination.annotation.PaginationConfig
+import chatox.platform.pagination.annotation.SortBy
+import chatox.platform.pagination.annotation.SortDirection
 import chatox.sticker.api.request.CreateStickerPackRequest
 import chatox.sticker.service.StickerPackService
 import org.springframework.security.access.prepost.PreAuthorize
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -43,5 +45,12 @@ class StickerPackController(private val stickerPackService: StickerPackService) 
     fun findStickerPackById(@PathVariable id: String) = stickerPackService.findStickerPackById(id)
 
     @GetMapping
-    fun searchStickerPacks(@RequestParam("name") name: String, paginationRequest: PaginationRequest) = stickerPackService.searchStickerPacks(name, paginationRequest)
+    @PaginationConfig(
+            sortBy = SortBy(allowed = ["createdAt", "id", "name"], defaultValue = "createdAt"),
+            sortingDirection = SortDirection(defaultValue = "desc")
+    )
+    fun searchStickerPacks(
+            @RequestParam(value = "name", required = false) name: String?,
+            paginationRequest: PaginationRequest
+    ) = stickerPackService.searchStickerPacks(name ?: "", paginationRequest)
 }
