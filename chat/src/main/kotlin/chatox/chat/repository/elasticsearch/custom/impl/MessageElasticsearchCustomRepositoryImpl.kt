4e0d1cabd84cf21993e9b2c428bb4b697f81cf57
Paper Plane <dagfortnewage@gmail.com>
2022-04-23 19:54:08 +0600

    fix: Fixed query for searching messages in chat (#9)

--- chat/src/main/kotlin/chatox/chat/repository/elasticsearch/custom/impl/MessageElasticsearchCustomRepositoryImpl.kt
+++ chat/src/main/kotlin/chatox/chat/repository/elasticsearch/custom/impl/MessageElasticsearchCustomRepositoryImpl.kt
@@ -2,10 +2,15 @@ package chatox.chat.repository.elasticsearch.custom.impl
 
 import chatox.chat.model.elasticsearch.MessageElasticsearch
 import chatox.chat.repository.elasticsearch.custom.MessageCustomElasticsearchRepository
+import org.elasticsearch.common.unit.Fuzziness
+import org.elasticsearch.index.query.QueryBuilder
+import org.elasticsearch.index.query.QueryBuilders
 import org.springframework.data.domain.Pageable
 import org.springframework.data.elasticsearch.core.ReactiveElasticsearchTemplate
 import org.springframework.data.elasticsearch.core.query.Criteria
 import org.springframework.data.elasticsearch.core.query.CriteriaQuery
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder
+import org.springframework.data.elasticsearch.core.query.Query
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
 
@@ -14,9 +19,11 @@ class MessageElasticsearchCustomRepositoryImpl(
         private val elasticsearchRestTemplate: ReactiveElasticsearchTemplate
 ) : MessageCustomElasticsearchRepository {
     override fun findByTextAndChatId(text: String, chatId: String, pageable: Pageable): Flux<MessageElasticsearch> {
-        val criteria = Criteria("text").fuzzy(text)
-                .and(Criteria("chatId").matches(chatId))
-        val query = CriteriaQuery(criteria, pageable)
+        val query = NativeSearchQueryBuilder()
+                .withQuery(QueryBuilders.matchPhrasePrefixQuery("text", text).slop(2))
+                .withFilter(QueryBuilders.matchQuery("chatId", chatId))
+                .withPageable(pageable)
+                .build();
 
         return find(query)
     }
@@ -29,7 +36,7 @@ class MessageElasticsearchCustomRepositoryImpl(
         return find(query)
     }
 
-    private fun find(query: CriteriaQuery): Flux<MessageElasticsearch> {
+    private fun find(query: Query): Flux<MessageElasticsearch> {
         return elasticsearchRestTemplate
                 .search(query, MessageElasticsearch::class.java)
                 .map { searchHit -> searchHit.content }
