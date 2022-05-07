00f5055c76795e06184fcd6b9f72055b1f86f3d3
Paper Plane <dagfortnewage@gmail.com>
2022-05-07 19:38:05 +0600

    refactor: (07.05.21) Moved search handling to SearchTextField component to make it reusable

--- client/src/ChatAppBar/components/ChatAppBarSearchInput.tsx
+++ client/src/ChatAppBar/components/ChatAppBarSearchInput.tsx
@@ -1,32 +1,7 @@
 import React, {FunctionComponent} from "react";
 import {observer} from "mobx-react";
-import {IconButton, InputAdornment, TextField, makeStyles, createStyles} from "@material-ui/core";
-import {Close, Search} from "@material-ui/icons";
-import {useLocalization, useStore} from "../../store";
-
-const useClasses = makeStyles(() => createStyles({
-    inputRoot: {
-        color: "inherit"
-    },
-    inputUnderline: {
-        borderBottomColor: "inherit",
-        "&:before": {
-            borderBottomColor: "inherit"
-        },
-        "&:hover:before": {
-            borderBottomColor: "inherit"
-        },
-        "&:hover": {
-            borderBottomColor: "inherit"
-        },
-        "&:hover:not(.Mui-Disabled):before": {
-            "borderBottomColor": "inherit"
-        },
-        "&:after": {
-            borderBottomColor: "inherit"
-        }
-    }
-}));
+import {useStore} from "../../store";
+import {SearchTextField} from "../../SearchTextField";
 
 export const ChatAppBarSearchInput: FunctionComponent = observer(() => {
     const {
@@ -36,37 +11,11 @@ export const ChatAppBarSearchInput: FunctionComponent = observer(() => {
             reset
         }
     } = useStore();
-    const {l} = useLocalization();
-    const classes = useClasses();
 
     return (
-        <TextField placeholder={l("common.search")}
-                   value={query}
-                   fullWidth
-                   style={{
-                       color: "inherit"
-                   }}
-                   InputProps={{
-                       startAdornment: (
-                           <InputAdornment position="start">
-                               <Search/>
-                           </InputAdornment>
-                       ),
-                       endAdornment: (
-                           <InputAdornment position="end">
-                               <IconButton onClick={reset}
-                                           color="inherit"
-                               >
-                                   <Close/>
-                               </IconButton>
-                           </InputAdornment>
-                       ),
-                       classes: {
-                           root: classes.inputRoot,
-                           underline: classes.inputUnderline
-                       }
-                   }}
-                   onChange={event => setQuery(event.target.value)}
+        <SearchTextField value={query}
+                         onQueryChange={setQuery}
+                         onClear={reset}
         />
-    );
+    )
 });
---
+++ client/src/SearchTextField/components/SearchTextField.tsx
@@ -0,0 +1,95 @@
+import React, {ChangeEvent, FunctionComponent} from "react";
+import {observer} from "mobx-react";
+import {createStyles, IconButton, InputAdornment, makeStyles, TextField, TextFieldProps} from "@material-ui/core";
+import {Close, Search} from "@material-ui/icons";
+import {useLocalization} from "../../store";
+
+const useClasses = makeStyles(() => createStyles({
+    inputRoot: {
+        color: "inherit"
+    },
+    inputUnderline: {
+        borderBottomColor: "inherit",
+        "&:before": {
+            borderBottomColor: "inherit"
+        },
+        "&:hover:before": {
+            borderBottomColor: "inherit"
+        },
+        "&:hover": {
+            borderBottomColor: "inherit"
+        },
+        "&:hover:not(.Mui-Disabled):before": {
+            "borderBottomColor": "inherit"
+        },
+        "&:after": {
+            borderBottomColor: "inherit"
+        }
+    }
+}));
+
+type SearchTextFieldProps = TextFieldProps & {
+    onClear?: () => void,
+    value: string,
+    onQueryChange?: (value: string) => void
+}
+
+export const SearchTextField: FunctionComponent<SearchTextFieldProps> = observer(({
+    onQueryChange,
+    onClear,
+    value,
+    variant,
+    fullWidth,
+    onChange,
+    classes,
+    ...other
+}) => {
+    const {l} = useLocalization();
+    const defaultClasses = useClasses();
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+        if (onChange) {
+            onChange(event);
+        }
+
+        if (onQueryChange) {
+            onQueryChange(event.target.value);
+        }
+    }
+
+    const handleClear = (): void => {
+        if (onClear) {
+            onClear();
+        }
+    }
+
+    return (
+        <TextField placeholder={l("common.search")}
+                   value={value}
+                   fullWidth
+                   InputProps={{
+                       startAdornment: (
+                           <InputAdornment position="start">
+                               <Search/>
+                           </InputAdornment>
+                       ),
+                       endAdornment: (
+                           <InputAdornment position="end">
+                               <IconButton onClick={handleClear}
+                                           color="inherit"
+                               >
+                                   <Close/>
+                               </IconButton>
+                           </InputAdornment>
+                       ),
+                       classes: {
+                           root: defaultClasses.inputRoot,
+                           underline: defaultClasses.inputUnderline,
+                           ...classes
+                       }
+                   }}
+                   onChange={handleChange}
+                   {...other}
+        />
+    );
+});
