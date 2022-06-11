093ad69f4b010fa5e2e8f595a83e6565bafa124e
Paper Plane <dagfortnewage@gmail.com>
2022-06-11 15:23:52 +0600

    fix: (11.06.22) Fixed broken styles of report pages

--- client/src/Report/components/ChatReportsActions.tsx
+++ client/src/Report/components/ChatReportsActions.tsx
@@ -1,14 +1,16 @@
-import React, {FunctionComponent} from "react";
+import React, {forwardRef} from "react";
 import {observer} from "mobx-react";
 import {ReportsActions} from "./ReportsActions";
 import {BanChatsCreatorsButton} from "./BanChatsCreatorsButton";
 import {DeleteChatsButton} from "./DeleteChatsButton";
 import {RejectReportsButton} from "./RejectReportsButton";
 
-export const ChatReportsActions: FunctionComponent = observer(() => (
-    <ReportsActions>
+const _ChatReportsActions = forwardRef<HTMLDivElement>((_, ref) => (
+    <ReportsActions ref={ref}>
         <BanChatsCreatorsButton/>
         <DeleteChatsButton/>
         <RejectReportsButton/>
     </ReportsActions>
 ));
+
+export const ChatReportsActions = observer(_ChatReportsActions);
