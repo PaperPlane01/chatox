278d075953f7ac974e0c05dffda6126b44e46c49
Paper Plane <dagfortnewage@gmail.com>
2023-01-26 11:10:12 +0600

    fix: (26.01.23) Wrong step was being set in createSetCreateNewEmailConfirmationCodeCallback (#98)

--- client/src/EmailUpdate/factories/createSetCreateNewEmailConfirmationCodeCallback.ts
+++ client/src/EmailUpdate/factories/createSetCreateNewEmailConfirmationCodeCallback.ts
@@ -2,4 +2,4 @@ import {createSetNextStepCallback} from "./createSetNextStepCallback";
 import {UpdateEmailStep} from "../types";
 
 export const createSetCreateNewEmailConfirmationCodeCallback
-    = createSetNextStepCallback(UpdateEmailStep.CREATE_CHANGE_EMAIL_CONFIRMATION_CODE);
+    = createSetNextStepCallback(UpdateEmailStep.CREATE_NEW_EMAIL_CONFIRMATION_CODE);
