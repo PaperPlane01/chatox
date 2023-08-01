082e5e6d76fa45bec691bbebcc9834e20b293740
Paper Plane <26180756+PaperPlane01@users.noreply.github.com>
2023-08-01 22:18:12 +0600

    refactor: (01.08.23) Removed unused imports

--- oauth2/src/main/java/chatox/oauth2/config/SecurityConfig.java
+++ oauth2/src/main/java/chatox/oauth2/config/SecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
