e4e61baf793b3832fcee0af557cee9b44e805a6e
Paper Plane <26180756+PaperPlane01@users.noreply.github.com>
2023-08-19 02:59:10 +0600

    feat: (19.08.23) Added validation constraints to `CreateRewardRequest` and `UpdateRewardRequest`

--- wallet/src/main/java/chatox/wallet/api/request/CreateRewardRequest.java
+++ wallet/src/main/java/chatox/wallet/api/request/CreateRewardRequest.java
@@ -1,8 +1,11 @@
 package chatox.wallet.api.request;
 
 import chatox.wallet.model.Currency;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,18 +23,30 @@ public class CreateRewardRequest {
     @NotNull
     private Currency currency;
     private String userId;
+
+    @Future
     private ZonedDateTime periodStart;
+
+    @Future
     private ZonedDateTime periodEnd;
 
     @NotNull
     @Positive
+    @Max(ValidationConstants.MAX_REWARD_VALUE)
     private BigDecimal minRewardValue;
 
     @NotNull
     @Positive
+    @Max(ValidationConstants.MAX_REWARD_VALUE)
     private BigDecimal maxRewardValue;
     private boolean useIntegersOnly;
     private ChronoUnit recurringPeriodUnit;
+
+    @Positive
+    @Max(ValidationConstants.MAX_RECURRING_PERIOD_VALUE)
     private Integer recurringPeriodValue;
     private boolean active;
+
+    @Size(max = 200)
+    private String name;
 }
--- wallet/src/main/java/chatox/wallet/api/request/UpdateRewardRequest.java
+++ wallet/src/main/java/chatox/wallet/api/request/UpdateRewardRequest.java
@@ -1,6 +1,8 @@
 package chatox.wallet.api.request;
 
 import chatox.wallet.model.Currency;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
@@ -20,18 +22,25 @@ public class UpdateRewardRequest {
     @NotNull
     private Currency currency;
     private String userId;
+
+    @Future
     private ZonedDateTime periodStart;
+
+    @Future
     private ZonedDateTime periodEnd;
 
     @NotNull
     @Positive
+    @Max(ValidationConstants.MAX_REWARD_VALUE)
     private BigDecimal minRewardValue;
 
     @NotNull
     @Positive
+    @Max(ValidationConstants.MAX_REWARD_VALUE)
     private BigDecimal maxRewardValue;
     private boolean useIntegersOnly;
     private ChronoUnit recurringPeriodUnit;
     private Integer recurringPeriodValue;
     private boolean active;
+    private String name;
 }
