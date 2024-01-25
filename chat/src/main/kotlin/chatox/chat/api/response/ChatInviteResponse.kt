5225e2840963abee4a864e7d9716d299a70bde74
Paper Plane <26180756+PaperPlane01@users.noreply.github.com>
2024-01-25 23:07:36 +0600

    feat: (25.01.23) Added usage info to `ChatInviteResponse` (#114)

--- chat/src/main/kotlin/chatox/chat/api/response/ChatInviteResponse.kt
+++ chat/src/main/kotlin/chatox/chat/api/response/ChatInviteResponse.kt
@@ -1,6 +1,11 @@
 package chatox.chat.api.response
 
+import chatox.chat.model.JoinChatAllowance
+import chatox.platform.security.VerificationLevel
+
 data class ChatInviteResponse(
         val id: String,
-        val chat: ChatResponse
+        val chat: ChatResponse,
+        val joinAllowanceSettings: Map<VerificationLevel, JoinChatAllowance>,
+        val usage: ChatInviteUsageResponse
 )
