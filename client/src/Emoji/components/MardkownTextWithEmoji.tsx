89967d1e0769a3d079eb111daa03f9890f35816f
Paper Plane <26180756+PaperPlane01@users.noreply.github.com>
2024-01-28 19:45:56 +0600

    feat: (28.01.24) Added styling to quotes

--- client/src/Emoji/components/MardkownTextWithEmoji.tsx
+++ client/src/Emoji/components/MardkownTextWithEmoji.tsx
@@ -1,6 +1,7 @@
 import React, {Fragment, FunctionComponent, ReactNode} from "react";
 import {observer} from "mobx-react";
-import {Typography} from "@mui/material";
+import {Theme, Typography} from "@mui/material";
+import {createStyles, makeStyles} from "@mui/styles";
 import ReactMarkdown from "react-markdown";
 import remarkBreaks from "remark-breaks";
 import {Element} from "hast";
@@ -15,6 +16,29 @@ interface MarkdownTextWithEmojiProps {
     uniqueId?: string
 }
 
+const useStyles = makeStyles((theme: Theme) => createStyles({
+    blockquote: {
+        marginBlockStart: 0,
+        marginBlockEnd: 0,
+        marginInlineStart: 0,
+        marginInlineEnd: 0,
+        paddingLeft: theme.spacing(4),
+        borderLeft: `${theme.spacing(0.5)} ${theme.palette.primary.main} solid`,
+        position: "relative",
+        "&::before": {
+            content: "'\u201C'",
+            position: "absolute",
+            fontSize: "4em",
+            color: theme.palette.primary.main,
+            left: 10,
+            top: -20
+        },
+        "&::after": {
+            content: "''"
+        }
+    }
+}));
+
 export const MarkdownTextWithEmoji: FunctionComponent<MarkdownTextWithEmojiProps> = observer(({
     text,
     emojiData,
@@ -22,6 +46,8 @@ export const MarkdownTextWithEmoji: FunctionComponent<MarkdownTextWithEmojiProps
     uniqueId
 }) => {
     const {parseEmoji} = useEmojiParser();
+    const classes = useStyles();
+
     const processEmoji = (node: Element, props: any): ReactNode | ReactNode[] => node.children.map(child => {
         if (child.type === "text") {
             const keyProvider: EmojiKeyProviderFunction | undefined = uniqueId
@@ -36,7 +62,7 @@ export const MarkdownTextWithEmoji: FunctionComponent<MarkdownTextWithEmojiProps
                 />
             );
         }
-    })
+    });
 
     return (
         <ReactMarkdown remarkPlugins={!disableRemarkBreaks ? [remarkBreaks] : []}
@@ -95,9 +121,15 @@ export const MarkdownTextWithEmoji: FunctionComponent<MarkdownTextWithEmojiProps
                                <h4>
                                    {processEmoji(node, props)}
                                </h4>
+                           ),
+                           blockquote: ({node, ...props}) => (
+                               <blockquote className={classes.blockquote}>
+                                   {processEmoji(node, props)}
+                               </blockquote>
                            )
                        }}
-                       children={text}
-        />
+        >
+            {text}
+        </ReactMarkdown>
     );
-})
+});
