60fecde568ddf966e0ae6fe5a32969320b658438
Paper Plane <26180756+PaperPlane01@users.noreply.github.com>
2024-02-09 21:34:49 +0600

    fix: (09.02.24) Fixed reward value generation when reward's lower and upper bound are equal

--- wallet/src/main/java/chatox/wallet/support/RandomNumbersGenerator.java
+++ wallet/src/main/java/chatox/wallet/support/RandomNumbersGenerator.java
@@ -11,6 +11,10 @@ public class RandomNumbersGenerator {
     private final SecureRandom random = new SecureRandom();
 
     public BigInteger randomBigInteger(BigInteger lowerBound, BigInteger upperBound) {
+        if (lowerBound.equals(upperBound)) {
+            return upperBound;
+        }
+
         var randomNumber = new BigInteger(upperBound.bitLength(), random);
 
         while (randomNumber.compareTo(lowerBound) <= 0 || randomNumber.compareTo(upperBound) >= 0) {
@@ -21,6 +25,10 @@ public class RandomNumbersGenerator {
     }
 
     public BigDecimal randomBigDecimal(BigDecimal lowerBound, BigDecimal upperBound) {
+        if (lowerBound.equals(upperBound)) {
+            return upperBound;
+        }
+
         return lowerBound.add(
                 BigDecimal.valueOf(random.nextDouble()).multiply(upperBound.subtract(lowerBound))
         );
