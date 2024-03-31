ce290dc3fdaef6c6bd7b964906098acea84b0a30
Paper Plane <26180756+PaperPlane01@users.noreply.github.com>
2024-03-31 13:02:06 +0600

    refactor: (31.03.24) Refactored parsing message editor type (#221)

--- client/src/Chat/types/MessageEditorType.ts
+++ client/src/Chat/types/MessageEditorType.ts
@@ -5,16 +5,14 @@ export enum MessageEditorType {
 	RICH_TEXT = "RICH_TEXT"
 }
 
+const VALUES = [MessageEditorType.PLAIN_TEXT, MessageEditorType.RICH_TEXT];
+
 export const parseMessageEditorType = (messageEditorType: string | null | undefined): MessageEditorType => {
 	if (!isDefined(messageEditorType)) {
 		return MessageEditorType.PLAIN_TEXT;
 	}
 
-	switch (messageEditorType.toUpperCase().trim()) {
-		case "RICH_TEXT":
-			return MessageEditorType.RICH_TEXT;
-		case "PLAIN_TEXT":
-		default:
-			return MessageEditorType.PLAIN_TEXT;
-	}
-}
+	messageEditorType = messageEditorType.toUpperCase().trim();
+
+	return VALUES.find(type => type === messageEditorType) ?? MessageEditorType.PLAIN_TEXT;
+};
