b28e16624fd364e323f5fe21403ee79d51fe1e2b
Paper Plane <26180756+PaperPlane01@users.noreply.github.com>
2024-05-25 17:05:31 +0600

    build: (25.05.24) Use `splitVendorChunkPlugin`

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,4 @@
-import {defineConfig} from "vite";
+import {defineConfig, splitVendorChunkPlugin} from "vite";
 import react from "@vitejs/plugin-react";
 import {comlink} from "vite-plugin-comlink";
 
@@ -6,6 +6,7 @@ export default defineConfig({
 	plugins: [
 		comlink(),
 		react(),
+		splitVendorChunkPlugin()
 	],
 	worker: {
 		plugins: () => ([
