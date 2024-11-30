1df2dfc75646f3c0805ba177fcf43356039d33ce
Paper Plane <26180756+PaperPlane01@users.noreply.github.com>
2024-11-30 13:57:16 +0600

    refactor: (30.11.24) Removed redundant `AbstractAuthenticationHolder` class

--- spring-utils/src/main/java/chatox/platform/security/web/AuthenticationHolder.java
+++ spring-utils/src/main/java/chatox/platform/security/web/AuthenticationHolder.java
@@ -3,11 +3,16 @@ package chatox.platform.security.web;
 import chatox.platform.security.jwt.JwtAuthentication;
 import chatox.platform.security.jwt.JwtPayload;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
 public interface AuthenticationHolder<U> {
-    Optional<JwtAuthentication> getCurrentAuthentication();
+    default Optional<JwtAuthentication> getCurrentAuthentication() {
+        return Optional.ofNullable(SecurityContextHolder.getContext())
+                .filter(JwtAuthentication.class::isInstance)
+                .map(JwtAuthentication.class::cast);
+    }
 
     default JwtAuthentication requireCurrentAuthentication() {
         return getCurrentAuthentication().orElseThrow(() -> new BadCredentialsException("Bad credentials"));
