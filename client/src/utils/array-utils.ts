a9965dbece212ab349efe8c61965d50b38d0c4d0
Paper Plane <26180756+PaperPlane01@users.noreply.github.com>
2026-02-05 22:04:26 +0000

    refactor: (05.05.26) Removed unsuded function from array-utils

--- client/src/utils/array-utils.ts
+++ client/src/utils/array-utils.ts
@@ -4,25 +4,6 @@ export const swapItems = <T>(array: T[], index1: number, index2: number): void =
 	[array[index1], array[index2]] = [array[index2], array[index1]];
 };
 
-export const splitToChunks = <T>(array: T[], chunkSize: number): Array<Array<T>> => {
-	const result: Array<Array<T>> = [];
-
-	for (let currentIndex = 0; currentIndex < array.length; currentIndex+=chunkSize) {
-		const currentChunk: T[] = [];
-		const currentChunkEnd = result.length + chunkSize - 1 >= array.length
-			? array.length - 1
-			: result.length + chunkSize - 1;
-
-		for (let innerIndex = currentIndex; innerIndex <= currentChunkEnd; innerIndex++) {
-			currentChunk.push(array[innerIndex]);
-		}
-
-		result.push(currentChunk);
-	}
-
-	return result;
-};
-
 export const createTuple = <T1, T2>(first: T1, second: T2): readonly [T1, T2] => {
 	return [first, second];
 };
