5dd242e48eca993d0611c4ad764fc197b2ebaca7
Paper Plane <26180756+PaperPlane01@users.noreply.github.com>
2026-02-07 12:52:51 +0000

    fix: (07.02.26) Fixed sticker editing dialog was not present on sticker pack import page (#240)

--- client/src/StickerPackForm/components/CreateStickerPackStep.tsx
+++ client/src/StickerPackForm/components/CreateStickerPackStep.tsx
@@ -1,8 +1,26 @@
-import React, {FunctionComponent} from "react";
+import React, {Fragment, FunctionComponent} from "react";
 import {observer} from "mobx-react";
+import {CreateStickerDialog} from "./CreateStickerDialog";
+import {EditStickerDialog} from "./EditStickerDialog";
 import {StickerPackFormFields} from "./StickerPackFormFields";
 import {getCreateStickerPackText} from "../utils";
+import {useStore} from "../../store";
 
-export const CreateStickerPackStep: FunctionComponent = observer(() =>
-	<StickerPackFormFields context="stickerPackCreation" getErrorText={getCreateStickerPackText}/>
-);
+export const CreateStickerPackStep: FunctionComponent = observer(() => {
+	const {
+		stickerPackCreation: {
+			editedSticker,
+			stickerUnderCreation
+		}
+	} = useStore();
+
+	return (
+		<Fragment>
+			<StickerPackFormFields context="stickerPackCreation" getErrorText={getCreateStickerPackText}/>
+			{stickerUnderCreation && (
+				<CreateStickerDialog stickerContainer={stickerUnderCreation} context="stickerPackCreation"/>
+			)}
+			{editedSticker && <EditStickerDialog stickerContainer={editedSticker} context="stickerPackCreation"/>}
+		</Fragment>
+	)
+});
