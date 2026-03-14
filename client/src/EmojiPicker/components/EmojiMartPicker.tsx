93a279d74343c20476d319d4c28fb1e0456d8b4d
Paper Plane <26180756+PaperPlane01@users.noreply.github.com>
2026-03-14 06:26:19 +0000

    fix: (14.03.26) Don't use lazy import for emoji-mart Picker

--- client/src/EmojiPicker/components/EmojiMartPicker.tsx
+++ client/src/EmojiPicker/components/EmojiMartPicker.tsx
@@ -1,13 +1,9 @@
-import React, { Suspense } from "react";
-const Picker = React.lazy(() => import("@emoji-mart/react"));
+import React from "react";
+import Picker from "@emoji-mart/react";
 import type EmojiMart from "emoji-mart";
 
 export function EmojiMartPicker<
     T extends keyof typeof EmojiMart.Picker.Props
 >(props: Record<T, typeof EmojiMart.Picker.Props[T]["value"]>) {
-    return (
-        <Suspense fallback={<></>}>
-            <Picker {...props} />
-        </Suspense>
-    );
-};
+    return <Picker {...props}/>;
+}
